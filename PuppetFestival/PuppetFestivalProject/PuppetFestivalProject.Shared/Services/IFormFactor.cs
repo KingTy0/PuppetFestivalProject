0e37bf6223aeab4199cbd7bcc408807541ea24f1
KingTy0 <tek.king999@gmail.com>
2026-02-26 21:31:01 -0600

    Delete PuppetFestival directory

--- PuppetFestival/PuppetFestivalProject/PuppetFestivalProject.Shared/Services/IFormFactor.cs
+++
@@ -1,7 +0,0 @@
-namespace PuppetFestivalProject.Shared.Services;
-
-public interface IFormFactor
-{
-    public string GetFormFactor();
-    public string GetPlatform();
-}
--- PuppetFestival/PuppetFestivalProject/PuppetFestivalProject/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PuppetFestivalProject;
-
-public partial class MainPage : ContentPage
-{
-    public MainPage()
-    {
-        InitializeComponent();
-    }
-}
